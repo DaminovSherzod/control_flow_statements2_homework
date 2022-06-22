2efa042b8fe83a86a62947039923aad497078869
Sherzod <daminovsherzod2002@gmail.com>
2022-06-22 15:21:30 +0500

    if06.py

--- if06.py
+++ if06.py
@@ -6,4 +6,28 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1 = n%10
+    n //= 10
+
+    x2 = n%10
+    n //= 10
+
+    x3 = n%10
+    n //= 10
+
+    x4 = n%10
+    n //= 10
+
+    x5 = n%10
+    n //= 10
+    if x1<=x5 and x2<=x5 and x3<=x5 and x4<=x5:
+        return '1'
+    if x1<=x4 and x2<=x4 and x3<=x4 and x5<=x4:
+        return '2'
+    if x1<=x3 and x2<=x3 and x4<=x3 and x5<=x3:
+        return '3'
+    if x1<=x2 and x3<=x2 and x4<=x2 and x5<=x2:
+        return '4'
+    if x2<=x1 and x3<=x1 and x4<=x1 and x5<=x1:
+        return '5'               
+print(main(12385))    
